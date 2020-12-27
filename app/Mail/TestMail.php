a4b5f6c5f43c3048f62d38c04bd0c233c5d3922a
Rob <dev@robmellett.com>
2020-12-27 17:16:31 +1000

    Fix the mail address

--- app/Mail/TestMail.php
+++ app/Mail/TestMail.php
@@ -9,7 +9,7 @@ class TestMail extends Mailable
     public function build()
     {
         return $this
-            ->to('mail.admin_email')
+            ->to(config('mail.admin_email'))
             ->subject('This is a test from the bref-test demo')
             ->view('emails.test');
     }
