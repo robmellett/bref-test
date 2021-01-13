b0502e0bc2e1efa22e52f51ec727f48c79296f7e
Rob <dev@robmellett.com>
2021-01-13 21:06:53 +1000

    populate default values

--- build/add-keys.sh
+++ build/add-keys.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
-aws ssm put-parameter --region ap-southeast-2 --name '/bref-dev/APP_KEY' --type String --value '${{ secrets.APP_KEY }}'
+aws ssm put-parameter --region ap-southeast-2 --name '/bref-dev/APP_NAME' --type String --value ''
+aws ssm put-parameter --region ap-southeast-2 --name '/bref-dev/APP_ENV' --type String --value 'production'
+aws ssm put-parameter --region ap-southeast-2 --name '/bref-dev/APP_DEBUG' --type String --value 'false'
 
-aws ssm put-parameter --region ap-southeast-2 --name '/bref-dev/APP_URL' --type String --value '${{ secrets.APP_URL }}'
+aws ssm put-parameter --region ap-southeast-2 --name '/bref-dev/APP_URL' --type String --value ''
+aws ssm put-parameter --region ap-southeast-2 --name '/bref-dev/AWS_PUBLIC_BUCKET' --type String --value ''
+aws ssm put-parameter --region ap-southeast-2 --name '/bref-dev/MAIL_MAILER' --type String --value ''
+aws ssm put-parameter --region ap-southeast-2 --name '/bref-dev/MAILGUN_DOMAIN' --type String --value ''
+aws ssm put-parameter --region ap-southeast-2 --name '/bref-dev/MAILGUN_SECRET' --type String --value ''
+aws ssm put-parameter --region ap-southeast-2 --name '/bref-dev/MAIL_FROM_ADDRESS' --type String --value ''
+aws ssm put-parameter --region ap-southeast-2 --name '/bref-dev/MAIL_FROM_NAME' --type String --value ''
+aws ssm put-parameter --region ap-southeast-2 --name '/bref-dev/MIX_ASSET_URL' --type String --value ''
