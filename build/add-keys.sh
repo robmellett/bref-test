ab57cfdb12fbcebd9bb796af0633358e4390093a
Rob <dev@robmellett.com>
2021-01-13 23:55:09 +1000

    Update default config

--- build/add-keys.sh
+++ build/add-keys.sh
@@ -3,9 +3,20 @@
 aws ssm put-parameter --region ap-southeast-2 --name '/bref-dev/APP_NAME' --type String --value ''
 aws ssm put-parameter --region ap-southeast-2 --name '/bref-dev/APP_ENV' --type String --value 'production'
 aws ssm put-parameter --region ap-southeast-2 --name '/bref-dev/APP_DEBUG' --type String --value 'false'
-
 aws ssm put-parameter --region ap-southeast-2 --name '/bref-dev/APP_URL' --type String --value ''
 aws ssm put-parameter --region ap-southeast-2 --name '/bref-dev/AWS_PUBLIC_BUCKET' --type String --value ''
+
+aws ssm put-parameter --region ap-southeast-2 --name '/bref-dev/SENTRY_LARAVEL_DSN' --type String --value ''
+aws ssm put-parameter --region ap-southeast-2 --name '/bref-dev/FILESYSTEM_DRIVER' --type String --value ''
+aws ssm put-parameter --region ap-southeast-2 --name '/bref-dev/FILESYSTEM_DRIVER_PUBLIC' --type String --value ''
+
+aws ssm put-parameter --region ap-southeast-2 --name '/bref-dev/DB_CONNECTION' --type String --value ''
+aws ssm put-parameter --region ap-southeast-2 --name '/bref-dev/DB_HOST' --type String --value ''
+aws ssm put-parameter --region ap-southeast-2 --name '/bref-dev/DB_PORT' --type String --value ''
+aws ssm put-parameter --region ap-southeast-2 --name '/bref-dev/DB_DATABASE' --type String --value ''
+aws ssm put-parameter --region ap-southeast-2 --name '/bref-dev/DB_USERNAME' --type String --value ''
+aws ssm put-parameter --region ap-southeast-2 --name '/bref-dev/DB_PASSWORD' --type String --value ''
+
 aws ssm put-parameter --region ap-southeast-2 --name '/bref-dev/MAIL_MAILER' --type String --value ''
 aws ssm put-parameter --region ap-southeast-2 --name '/bref-dev/MAILGUN_DOMAIN' --type String --value ''
 aws ssm put-parameter --region ap-southeast-2 --name '/bref-dev/MAILGUN_SECRET' --type String --value ''
